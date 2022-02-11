8b8a8f2bd299762a246f93949865cd54f25a2c6a
Naya070 <nayarith070@gmail.com>
2022-02-11 15:08:16 -0400

    first commit

---
+++ src/JavaGráficos.java
@@ -0,0 +1,47 @@
+import javax.swing.JFrame;
+//import javax.swing.JPanel;
+ 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+ 
+public class JavaGráficos
+{
+ 
+    public static void main(String[] args) 
+    {       
+        
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("IPhone 5s", new Double(20));
+        dataset.setValue("SamSung Grand", new Double(20));
+        dataset.setValue("MotoG", new Double(40));
+        dataset.setValue("Nokia Lumia", new Double(10));
+ 
+        
+        JFreeChart chart = ChartFactory.createPieChart(// char t
+                
+                "Mobile Sales",// title                                                                     
+                dataset, // data
+                true, // include legend
+                true, false);
+        
+        
+        ChartPanel panel= new ChartPanel(chart);
+        
+        
+        
+        
+        //Creamos la ventana
+        JFrame ventana = new JFrame("Grafica");
+        ventana.setVisible(true);
+        ventana.setSize(800, 600);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        ventana.add(panel);
+        
+ 
+    }
+ 
+}
